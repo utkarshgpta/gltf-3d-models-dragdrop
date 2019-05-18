f024084d8b26110b6f4cbf8d4181ff429f7f3f20
Utkarsh Gupta <utkarshg1408@gmail.com>
2019-05-19 03:54:52 +0530

    Updated Heroku Database configs

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ config.webhost = 'https://gltf-drag-drop.herokuapp.com/';
 // your MongoDB host and database name
 config.db.localhost = 'mongodb://localhost';
 config.db.port = 27017;
-config.db.name = 'v045bbsg';
+config.db.name = 'heroku_v045bbsg';
 
 module.exports = config;
