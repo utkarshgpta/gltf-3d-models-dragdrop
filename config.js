a188ad549219db01fe461cb0a7fbc8aa664f4a78
Utkarsh Gupta <utkarshg1408@gmail.com>
2019-05-19 03:32:41 +0530

    Added configurations for MongoDB

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 var config = {};
 
 config.db = {};
-// config.webhost = ' https://slash-url.herokuapp.com/';
+// the URL shortening host - shortened URLs will be this + baseHash ID
+
+// i.e.: http://localhost:5000/3Ys
+config.webhost = 'https://gltf-drag-drop.herokuapp.com/';
 
 // your MongoDB host and database name
 config.db.localhost = 'mongodb://localhost';
 config.db.port = 27017;
-// config.db.name = 'heroku_6srjdlq5';
+config.db.name = 'v045bbsg';
 
 module.exports = config;
